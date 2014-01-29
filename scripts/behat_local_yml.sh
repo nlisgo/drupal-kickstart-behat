451dc90b9dad5162f72c036d5012727effbff733
nlisgo <nathan@aeopus.com>
2014-01-29 01:08:04 +0000

    Introduce saucelabs

--- scripts/behat_local_yml.sh
+++ scripts/behat_local_yml.sh
@@ -5,7 +5,10 @@ sudo echo "default:
   extensions:
     Behat\MinkExtension\Extension:
       base_url: 'http://127.0.0.1:80'
+      selenium2:
+        wd_host: $SAUCE_USERNAME:$SAUCE_ACCESS_KEY@ondemand.saucelabs.com/wd/hub
     Drupal\DrupalExtension\Extension:
       blackbox: ~
       drupal:
         drupal_root: '$(pwd)/kickstart'" | sudo tee "$(pwd)/tests/behat/behat.local.yml" > /dev/null
+cat "$(pwd)/tests/behat/behat.local.yml"
